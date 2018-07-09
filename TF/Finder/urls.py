5f807d50ece69beffae1e0a47f081ec6bd7c7c4c
AliEdalat <ali.edalat.77@gmail.com>
2018-07-10 00:26:31 +0430

    login completed and signup is near to end

--- TF/Finder/urls.py
+++ TF/Finder/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
+    path('login/aut', views.authentication, name='authentication'),
+    path('login/signup', views.signup, name='signup'),
+    path('login/signup_new_user', views.signup_new_user, name='signup_new_user'),
 ]
--- TF/Finder/views.py
+++ TF/Finder/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-
+from .models import Users
+from django.utils import timezone
 
 def login(request):
     return render(request, '/home/ali/talent-finder/TF/Finder/templates/w.html')
+
+def authentication(request):
+	name=request.GET.get('uname',default=None)
+	password=request.GET.get('psw',default=None)
+	print(name,password)
+	latest_users_list = Users.objects.order_by('-created_date')
+	print(latest_users_list[0].username)
+	for x in latest_users_list:
+		if x.username==name and x.password==password:
+			return render(request, '/home/ali/talent-finder/TF/Finder/templates/signup.html')
+		else:
+			return render(request, '/home/ali/talent-finder/TF/Finder/templates/w.html')
+
+def signup(request):
+	return render(request, '/home/ali/talent-finder/TF/Finder/templates/signup.html')
+
+def signup_new_user(request):
+	name=request.GET.get('uname',default=None)
+	password=request.GET.get('psw',default=None)
+	new_user=Users(username=name , password=password,Email="",created_date=timezone.now())
+	print(name,password)
+	new_user.save()
+	print(Users.objects.all())
+	return render(request, '/home/ali/talent-finder/TF/Finder/templates/signup.html')
